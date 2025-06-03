dfeb6809cee7741809b056303f93f1eeb60e7380
Pradeep Ranjan <pradeep.ranjan7837@gmail.com>
2025-06-03 14:51:55 -0500

    Added new tab opening

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -7,7 +7,7 @@ function ProjectCard({ project }) {
       <p>{project.description}</p>
       <div className="buttons">
         <a href={project.githubLink} target="_blank" rel="noreferrer">GitHub Code</a>
-        <a href={project.demoLink} target="_blank" rel="noreferrer">Live Demo</a>
+        {/* <a href={project.demoLink} target="_blank" rel="noreferrer">Live Demo</a> */}
 
       </div>
     </div>
